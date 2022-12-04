5babeedd9a1fc6ada1b9f7de97553e7c589b0227
Tri Tran <ttran456@wch135-17.cs.ucr.edu>
2022-12-04 00:10:46 -0800

    Finished implementing triggers

--- sql/src/create_tables.sql
+++ sql/src/create_tables.sql
@@ -77,8 +77,78 @@ CREATE TABLE ProductUpdates (
                         	FOREIGN KEY(storeID, productName) REFERENCES Product(storeID, productName)
 );
 
+
+
 CREATE OR REPLACE FUNCTION calculate_distance(lat1 decimal, long1 decimal, lat2 decimal, long2 decimal)
 RETURNS decimal AS $dist$
 BEGIN RETURN sqrt((lat1 - lat2) * (lat1 - lat2) + (long1 - long2) * (long1 - long2));
 END;
 $dist$ LANGUAGE plpgsql;
+
+/* Trigger part */
+/*
+
+DROP SEQUENCE IF EXISTS order_num;
+CREATE SEQUENCE order_num START WITH 501;
+
+DROP SEQUENCE IF EXISTS update_num;
+CREATE SEQUENCE update_num START WITH 51;
+
+DROP SEQUENCE IF EXISTS user_num;
+CREATE SEQUENCE user_num START WITH 101;
+
+CREATE OR REPLACE LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION set_order_number()
+RETURNS "trigger" AS
+$BODY$
+BEGIN
+	NEW.orderNumber = nextval('order_num');
+	return NEW;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+CREATE OR REPLACE FUNCTION set_update_number()
+RETURNS "trigger" AS
+$BODY$
+BEGIN
+        NEW.updateNumber = nextval('order_num');
+        return NEW;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+CREATE OR REPLACE FUNCTION set_user_number()
+RETURNS "trigger" AS
+$BODY$
+BEGIN
+        NEW.userID = nextval('order_num');
+        return NEW;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+
+DROP TRIGGER IF EXISTS set_order_number_trigger ON Orders;
+DROP Trigger IF exists set_update_number_trigger ON ProductUpdates;
+DROP Trigger IF exists set_user_number_trigger ON Users;
+
+CREATE TRIGGER set_order_number_trigger
+BEFORE INSERT
+ON Orders
+FOR EACH ROW
+EXECUTE PROCEDURE set_order_number();
+
+CREATE TRIGGER set_update_number_trigger
+BEFORE INSERT
+ON ProductUpdates
+FOR EACH ROW
+EXECUTE PROCEDURE set_update_number();
+
+CREATE TRIGGER set_user_number_trigger
+BEFORE INSERT
+ON Users
+FOR EACH ROW
+EXECUTE PROCEDURE set_user_number();
+*/
