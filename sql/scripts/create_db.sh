5c7e716888acb55ae3dc1eb98e897c28371020de
Tri Tran <ttran456@wch135-17.cs.ucr.edu>
2022-12-06 09:46:31 -0800

    Final edit

--- sql/scripts/create_db.sh
+++ sql/scripts/create_db.sh
@@ -3,4 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/create_tables.sql
 psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/load_data.sql
 psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/create_indexes.sql
+psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/Triggers.sql
+
 
--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -1,32 +1,32 @@
 COPY Users
-FROM 'users.csv'
+FROM '/extra/ttran456/RetailStore/data/users.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE users_userID_seq RESTART 101;
 
 COPY Store
-FROM 'stores.csv'
+FROM '/extra/ttran456/RetailStore/data/stores.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Product
-FROM 'products.csv'
+FROM '/extra/ttran456/RetailStore/data/products.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Warehouse
-FROM 'warehouse.csv'
+FROM '/extra/ttran456/RetailStore/data/warehouse.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Orders
-FROM 'orders.csv'
+FROM '/extra/ttran456/RetailStore/data/orders.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE orders_orderNumber_seq RESTART 501;
 
 
 COPY ProductSupplyRequests
-FROM 'productSupplyRequests.csv'
+FROM '/extra/ttran456/RetailStore/data/productSupplyRequests.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productsupplyrequests_requestNumber_seq RESTART 11;
 
 COPY ProductUpdates
-FROM 'productUpdates.csv'
+FROM '/extra/ttran456/RetailStore/data/productUpdates.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productupdates_updateNumber_seq RESTART 51;
